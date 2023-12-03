4aaab473ec1486bbb5c14877fbaf10ad62e82bc6
zena123 <zinab.jom3a@gmail.com>
2023-12-03 20:34:20 +0100

    make orders anatomic transaction

--- checkot/views.py
+++ checkot/views.py
@@ -1,5 +1,6 @@
 import decimal
 
+from django.db import transaction
 from django.shortcuts import render
 from rest_framework import exceptions
 from rest_framework.response import Response
@@ -17,34 +18,42 @@ class LinkAPIView(APIView):
 
 
 class OrderAPIView(APIView):
+    @transaction.atomic
     def post(self, request):
         data = request.data
         link = Link.objects.filter(code=data['code']).first()
         if not link:
             raise exceptions.APIException("invalid code")
+        try:
+            order = Order()
+            order.code = link.code
+            order.user_id = link.user.id
+            order.ambassador_email = link.user.email
+            order.first_name = data['first_name']
+            order.last_name = data['last_name']
+            order.email = data['email']
+            order.address = data['address']
+            order.country = data['country']
+            order.city = data['city']
+            order.zip = data['zip']
+            order.save()
+            for item in data['products']:
+                product = Product.objects.filter(pk=item['product_id']).first()
+                quantity = decimal.Decimal(item['quantity'])
+                order_item = OrderItem()
+                order_item.order = order
+                order_item.product_title = product.title
+                order_item.price = product.price
+                order_item.quantity = quantity
+                order_item.ambassador_revenue = decimal.Decimal(.1) * product.price * quantity
+                order_item.admin_revenue = decimal.Decimal(.9) * product.price * quantity
+                order_item.save()
+            return Response(
+                {"message": "success!"})
+        except Exception:
+            transaction.rollback()
 
-        order = Order.objects.create(
-            code=link.code,
-            user_id=link.user.id,
-            ambassador_email=link.user.email,
-            first_name=data['first_name'],
-            last_name=data['last_name'],
-            email=data['email'],
-            address=data['address'],
-            country=data['country'],
-            city=data['city'],
-            zip=data['zip'],
-        )
-        for item in data['products']:
-            product = Product.objects.filter(pk=item['product_id']).first()
-            quantity = decimal.Decimal(item['quantity'])
-            order_item = OrderItem()
-            order_item.order = order
-            order_item.product_title = product.title
-            order_item.price = product.price
-            order_item.quantity = quantity
-            order_item.ambassador_revenue = decimal.Decimal(.1) * product.price * quantity
-            order_item.admin_revenue = decimal.Decimal(.9) * product.price * quantity
-            order_item.save()
-            return Response("success!")
+        return Response(
+            {"message": "error occurred!"})
+
 
