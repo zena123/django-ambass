7f33fdfc71195c8b2b17ea0c9ef3f551ab30d96e
zena123 <zinab.jom3a@gmail.com>
2024-01-15 22:14:53 +0100

    add bootstarp, html, and basic skeleton

---
+++ react-admin/src/components/Nav.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Nav = () =>{
+    return(
+        <header className="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
+            <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Company name</a>
+        
+            <ul className="navbar-nav flex-row d-md-none">
+            <li className="nav-item text-nowrap">
+                <button className="nav-link px-3 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSearch" aria-controls="navbarSearch" aria-expanded="false" aria-label="Toggle search">
+                <svg className="bi"><use xlinkHref="#search"/></svg>
+                </button>
+            </li>
+            <li className="nav-item text-nowrap">
+            </li>
+            </ul>
+        
+            <div id="navbarSearch" className="navbar-search w-100 collapse">
+            </div>
+      </header>
+        );
+};
+
+export default Nav;
