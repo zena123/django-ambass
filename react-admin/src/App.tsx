7024d9a6df84e162563332c8e8d81b79fc7968c1
zena123 <zinab.jom3a@gmail.com>
2024-01-15 23:13:31 +0100

    add routers, login form

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -1,48 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Nav from './components/Nav';
-import Menu from './components/Menu';
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import Users from './pages/Users';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
+      <BrowserRouter>
+      <Routes>
+        <Route path= {'/'}  Component={Users}></Route>
+        <Route path={'/login'} Component={Login}></Route>
+        <Route path={'/register'} Component={Register}></Route>
+        </Routes>
+      </BrowserRouter>
 
-
-<div className="container-fluid">
-  <div className="row">
-
-    <Menu/>
-    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-
-      <h2>Section title</h2>
-      <div className="table-responsive small">
-        <table className="table table-striped table-sm">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1,001</td>
-              <td>random</td>
-              <td>data</td>
-              <td>placeholder</td>
-              <td>text</td>
-            </tr>
-
-          </tbody>
-        </table>
-      </div>
-    </main>
-  </div>
-</div>
     </div>
   );
 }
