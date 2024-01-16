18ee66c60a93ec5fcf9ca2d18427cc5623966d1b
zena123 <zinab.jom3a@gmail.com>
2024-01-16 10:41:37 +0100

    add register component

--- react-admin/src/pages/Login.tsx
+++ react-admin/src/pages/Login.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import '../login.css';
 
+
+// state component
 const Login = () => {
     return (
-        <div><form>
-        <img className="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
-        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-    
-        <div className="form-floating">
-          <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-          <label htmlFor="floatingInput">Email address</label>
-        </div>
-        <div className="form-floating">
-          <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-          <label htmlFor="floatingPassword">Password</label>
-        </div>
-    
-        <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
-      </form></div>
+        <div>
+          <form>
+            <img className="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+        
+            <div className="form-floating">
+              <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+              <label htmlFor="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+              <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+              <label htmlFor="floatingPassword">Password</label>
+            </div>
+        
+            <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+        </form>
+      </div>
     );
 };
 
--- react-admin/src/pages/Register.tsx
+++ react-admin/src/pages/Register.tsx
@@ -1,11 +1,82 @@
-import React from "react";
+import axios from "axios";
+import {Component} from "react";
+import React from 'react';
+import { Navigate } from 'react-router-dom';
 
-const Register = () =>{
-    return (
-        <div>
-            Register
-        </div>
-    );
-};
 
+
+// class Componenet
+class Register extends Component{
+    firstName = '';
+    lastName = '';
+    email = '';
+    password = '';
+    conrimPassword = '';
+
+    state = {
+        redirect: false,
+    }
+
+    submit = async(e:React.SyntheticEvent) =>{
+        e.preventDefault();
+       await axios.post("http://localhost:8000/api/admin/register/", {
+        first_name: this.firstName,
+        last_name: this.lastName,
+        email :this.email,
+        password: this.password,
+        confirm_password: this.conrimPassword
+       });
+
+       this.setState({
+        redirect: true
+       })
+    }
+        render(){
+            if(this.state.redirect){
+                return <Navigate to={"/login"}/>;
+            }
+        return (
+            <main className="form-signin">
+            <form onSubmit={this.submit}>
+                <img className="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
+                <h1 className="h3 mb-3 fw-normal">register</h1>
+
+                <div className="form-floating">
+                <input type="text" className="form-control" placeholder="First name"
+                onChange={e => {this.firstName = e.target.value;}}
+                />
+                <label htmlFor="floatingInput">First name</label>
+                </div>
+
+                <div className="form-floating">
+                <input type="text" className="form-control"  placeholder="Last name"
+                onChange={e => {this.lastName = e.target.value;}}/>
+                <label htmlFor="floatingInput">Last name</label>
+                </div>
+            
+                <div className="form-floating">
+                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"
+                onChange={e => {this.email = e.target.value;}}/>
+                <label htmlFor="floatingInput">Email address</label>
+                </div>
+                <div className="form-floating">
+                <input type="password" className="form-control"  placeholder="Password"
+                onChange={e => {this.password = e.target.value;}}/>
+                <label htmlFor="floatingPassword">Password</label>
+                </div>
+
+                <div className="form-floating">
+                <input type="password" className="form-control"  placeholder="Password confirm"
+                onChange={e => {this.conrimPassword = e.target.value;}}/>
+                <label htmlFor="floatingPassword">Password confirm </label>
+                </div>
+                <button className="btn btn-primary w-100 py-2" type="submit">register</button>
+            </form>
+            </main>
+        );
+        
+    }
+
+
+}
 export default Register;
