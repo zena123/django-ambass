89c9b92dcddf8e762158d560ec2950b51124a676
zena123 <zinab.jom3a@gmail.com>
2024-01-16 11:03:14 +0100

    add login state"not class" component

--- react-admin/src/pages/Login.tsx
+++ react-admin/src/pages/Login.tsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import '../login.css';
+import axios from "axios";
+import { Navigate } from 'react-router-dom';
 
 
-// state component
+// state component, we will use react hooks here instead od class component 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
+
+  const submit = async(e:React.SyntheticEvent) => {
+    e.preventDefault(); // prevent the page to be refreshed after submitting the form
+    await axios.post("login/", {
+      email,
+      password   //since key and value are same => we can send like that
+    },{withCredentials:true}); // the only way to get the cookies
+    setRedirect(true);
+  }
+  if(redirect){
+        return <Navigate to={"/"}/>;
+    }
     return (
         <div>
-          <form>
+          <form onSubmit={submit}>
             <img className="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
             <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
         
             <div className="form-floating">
-              <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+              <input type="email" className="form-control"  placeholder="name@example.com"
+              onChange={e => setEmail(e.target.value)}/>
               <label htmlFor="floatingInput">Email address</label>
             </div>
             <div className="form-floating">
-              <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+              <input type="password" className="form-control"  placeholder="Password"
+              onChange={e => setPassword(e.target.value)}/>
               <label htmlFor="floatingPassword">Password</label>
             </div>
         
--- react-admin/src/pages/Register.tsx
+++ react-admin/src/pages/Register.tsx
@@ -19,7 +19,7 @@ class Register extends Component{
 
     submit = async(e:React.SyntheticEvent) =>{
         e.preventDefault();
-       await axios.post("http://localhost:8000/api/admin/register/", {
+       await axios.post("register/", {
         first_name: this.firstName,
         last_name: this.lastName,
         email :this.email,
